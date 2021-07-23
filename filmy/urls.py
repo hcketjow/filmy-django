80cf27df58500c13483177723c264cd62837085a
Wojciech <hcketjow@gmail.com>
2021-07-23 09:05:54 +0200

    super

--- filmy/urls.py
+++ filmy/urls.py
@@ -13,7 +13,8 @@ router.register(r'filmy', FilmView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('filmy/', include('filmyweb.urls')),
+    # filmy/
+    path('', include('filmyweb.urls')),
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('', include(router.urls)),
