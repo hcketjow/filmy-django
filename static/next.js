5d0d8656f9a2d39f27ed5743dd9c0ec51c75deaa
Wojciech <hcketjow@gmail.com>
2021-08-01 22:19:50 +0200

    over

--- static/next.js
+++ static/next.js
@@ -44,8 +44,3 @@
 //     visible +=3 
 //     handleGetData()
 // })
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-var collapseList = collapseElementList.map(function (collapseEl) {
-  return new bootstrap.Collapse(collapseEl)
-})
-console.log()
